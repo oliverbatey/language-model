9f71043f958a1af1fe87e98b78dec0c2e83b6419
Oliver Batey <oliverbatey@Mac.Home>
2026-06-12 14:06:44 +0100

    Implemented RMSNorm and updated adapters.

--- cs336_basics/layers.py
+++ cs336_basics/layers.py
@@ -1,5 +1,5 @@
 import torch
-from einops import einsum, rearrange
+from einops import einsum, rearrange, repeat
 from math import sqrt
 
 class Linear(torch.nn.Module):
@@ -44,19 +44,29 @@ class Embedding(torch.nn.Module):
         # embedding_matrix.shape = (vocab_size, d_model)
         # token_ids.shape = (batch_size, sequence_length)
         # output.shape = (batch_size, sequence_length, d_model)
-
-        # Sequence of token_id values for every sequence in batch, flattened into a rank-1 tensor
-        tokens_flattened = rearrange(token_ids, "batch_size sequence_length -> (batch_size sequence_length)")
-
-        # list of embeddings shape (sequence_length * batch_size, d_model), rank-2
-        embeddings_indexed = torch.index_select(self.embedding_matrix, 0, tokens_flattened)
-
-        # rearrange to rank-3 shape (batch_size, sequence_length, d_model)
-        # return rearrange(
-        #     embeddings_indexed,
-        #     "(batch_size sequence_length) d_model -> batch_size sequence_length d_model",
-        #     sequence_length=token_ids.shape[1]
-        # )
         return self.embedding_matrix[token_ids]
         
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, d_model: int, eps: float=1e-5, device=None, dtype=None):
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        self.device = device
+        self.dtype = dtype
+        self.gain = torch.nn.Parameter(torch.ones(d_model))
+    
+    def _rms(self, a: torch.Tensor) -> float:
+        return torch.sqrt((1 / self.d_model) * einsum(a, a, "... d_model, ... d_model -> ...") + self.eps)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x.shape = (batch, sequence_length, d_model)
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        # Pytorch would broadcast rms automatically, but I've included it for explicit clarity.
+        rms = repeat(self._rms(x), "... -> ... d_model", d_model=self.d_model)
+        return torch.multiply(torch.div(x, rms), self.gain).to(in_dtype)
+
+
+
 
--- tests/adapters.py
+++ tests/adapters.py
@@ -10,7 +10,11 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 from cs336_basics.tokenizer import Tokenizer, train_bpe
-from cs336_basics.layers import Embedding, Linear
+from cs336_basics.layers import (
+    Embedding,
+    Linear,
+    RMSNorm,
+)
 
 
 def run_linear(
@@ -385,7 +389,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    layer = RMSNorm(d_model=d_model, eps=eps)
+    layer.load_state_dict({"gain": weights})
+    return layer(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
