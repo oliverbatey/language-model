d639bbd41bfc264b4598655f80b27c1977291a8c
Oliver Batey <oliverbatey@Mac.Home>
2026-06-12 10:54:32 +0100

    Implemented linear and embedding layers; updated test adapters.

---
+++ cs336_basics/layers.py
@@ -0,0 +1,62 @@
+import torch
+from einops import einsum, rearrange
+from math import sqrt
+
+class Linear(torch.nn.Module):
+    def __init__(self, d_in: int, d_out: int, device: torch.device | None=None, dtype: torch.dtype | None=None):
+        super().__init__()
+        self.d_in = d_in
+        self.d_out = d_out
+        self.device = device  # The device to store layer parameters on
+        self.dtype = dtype
+        self._initialise_weights()
+    
+    @staticmethod
+    def _initial_weight_std(d_in: int, d_out: int) -> float:
+        return sqrt(2/(d_in + d_out))
+    
+    def _initialise_weights(self):
+        W_std = self._initial_weight_std(self.d_in, self.d_out)
+        self.W = torch.nn.Parameter(
+            torch.nn.init.trunc_normal_(torch.empty(self.d_out, self.d_in), mean=0, std=W_std, a=-3*W_std, b=3*W_std)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x.shape = (batch_size, sequence_length, d_model)
+        # W.shape = (d_out, d_in)
+        # We need sum over the third dimension of x which means we need d_model==d_in
+        Wt = rearrange(self.W, "d_model d_out -> d_out d_model")
+        return einsum(x, Wt, "batch_size sequence_length d_model, d_model d_out -> batch_size sequence_length d_out")
+
+
+class Embedding(torch.nn.Module):
+    def __init__(self, num_embeddings: int, embedding_dim: int, device=None, dtype=None):
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.device = device
+        self.dtype = dtype
+        self.embedding_matrix = torch.nn.Parameter(
+            torch.nn.init.trunc_normal_(torch.empty(num_embeddings, embedding_dim), mean=0.0, std=1.0, a=-3.0, b=3.0)
+        )
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        # embedding_matrix.shape = (vocab_size, d_model)
+        # token_ids.shape = (batch_size, sequence_length)
+        # output.shape = (batch_size, sequence_length, d_model)
+
+        # Sequence of token_id values for every sequence in batch, flattened into a rank-1 tensor
+        tokens_flattened = rearrange(token_ids, "batch_size sequence_length -> (batch_size sequence_length)")
+
+        # list of embeddings shape (sequence_length * batch_size, d_model), rank-2
+        embeddings_indexed = torch.index_select(self.embedding_matrix, 0, tokens_flattened)
+
+        # rearrange to rank-3 shape (batch_size, sequence_length, d_model)
+        # return rearrange(
+        #     embeddings_indexed,
+        #     "(batch_size sequence_length) d_model -> batch_size sequence_length d_model",
+        #     sequence_length=token_ids.shape[1]
+        # )
+        return self.embedding_matrix[token_ids]
+        
+
--- tests/adapters.py
+++ tests/adapters.py
@@ -10,6 +10,7 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 from cs336_basics.tokenizer import Tokenizer, train_bpe
+from cs336_basics.layers import Embedding, Linear
 
 
 def run_linear(
@@ -30,8 +31,11 @@ def run_linear(
     Returns:
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
+    layer = Linear(d_in, d_out)
+    layer.load_state_dict({"W": weights})
+    return layer(in_features)
 
-    raise NotImplementedError
+
 
 
 def run_embedding(
@@ -52,8 +56,10 @@ def run_embedding(
     Returns:
         Float[Tensor, "... d_model"]: Batch of embeddings returned by your Embedding layer.
     """
+    embedding_layer = Embedding(num_embeddings=vocab_size, embedding_dim=d_model)
+    embedding_layer.embedding_matrix = torch.nn.Parameter(weights)
 
-    raise NotImplementedError
+    return embedding_layer(token_ids)
 
 
 def run_swiglu(
