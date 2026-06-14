fd280e0cfde946a453d0f619cf8d7aa4d7f2ae58
Oliver Batey <oliverbatey@Olivers-MacBook-Pro.local>
2026-06-14 16:35:32 +0100

    Added softmax and scaled_dot_product_attention functions. Updated adapters. Renamed cs223_basics to language_model

---
+++ language_model/functions.py
@@ -0,0 +1,31 @@
+import torch
+from einops import einsum, rearrange, repeat
+from math import sqrt
+
+
+def softmax(x: torch.Tensor, dim: int) -> torch.Tensor:
+    x = torch.exp(x - torch.max(x, dim=dim, keepdim=True).values)
+    return x / torch.sum(x, dim=dim, keepdim=True)
+
+
+def scaled_dot_product_attention(Q, K, V, mask) -> torch.Tensor:
+    """
+    Given key (K), query (Q), and value (V) tensors, return
+    the scaled dot product attention implementation.
+
+    Args:
+        Q (Float[Tensor, " ... queries d_k"]): Query tensor
+        K (Float[Tensor, " ... keys d_k"]): Key tensor
+        V (Float[Tensor, " ... values d_v"]): Values tensor
+        mask (Bool[Tensor, " ... queries keys"] | None): Mask tensor
+    Returns:
+        Float[Tensor, " ... queries d_v"]: Output o
+    """
+    QK_t = einsum(Q, K, "... queries d_k, ... keys d_k -> ... queries keys")
+    logits = QK_t / sqrt(K.shape[-1])
+    if mask is not None:
+        logits = logits.masked_fill(~mask, float("-inf")) # False values should be masked and set to -inf, so invert mask
+    scores = softmax(logits, dim=-1)
+    return einsum(scores, V, "... queries keys, ... keys d_v -> ... queries d_v")
+
+
--- tests/adapters.py
+++ tests/adapters.py
@@ -9,13 +9,17 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
-from cs336_basics.tokenizer import Tokenizer, train_bpe
-from cs336_basics.layers import (
+from language_model.tokenizer import Tokenizer, train_bpe
+from language_model.layers import (
     Embedding,
     Linear,
     RMSNorm,
     SwiGLU,
     RotaryPositionalEmbedding
+)
+from language_model.functions import (
+    scaled_dot_product_attention,
+    softmax
 )
 
 
@@ -121,7 +125,8 @@ def run_scaled_dot_product_attention(
     Returns:
         Float[Tensor, " ... queries d_v"]: Output of SDPA
     """
-    raise NotImplementedError
+    
+    return scaled_dot_product_attention(Q=Q, K=K, V=V, mask=mask)
 
 
 def run_multihead_self_attention(
@@ -451,7 +456,7 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    return softmax(in_features, dim)
 
 
 def run_cross_entropy(
